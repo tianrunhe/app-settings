89444d09d8bf659c2783ee13bfb80ccb4627d7d4
Bruce Tian <tianrunhe@gmail.com>
2020-05-16 16:52:59 -0700

    Add requirements.txt and add main() method in import_settings_to_airtable.py

--- import_settings_to_airtable.py
+++ import_settings_to_airtable.py
@@ -10,20 +10,26 @@ airtables = {
     'Mac': Airtable(os.getenv('AIRTABLE_BASE_ID'), 'iPhone')
 }
 
-schemas = {}
-for item in Path('schema').iterdir():
-    if item.is_file():
-        with open(item) as schema_json_file:
-            schema = json.load(schema_json_file)
-            schemas[os.path.splitext(item.name)[0]] = schema
 
-for os_name in schemas:
-    airtable = airtables[os_name]
-    for item in Path(f'settings/{os_name}').iterdir():
+def main():
+    schemas = {}
+    for item in Path('schema').iterdir():
         if item.is_file():
-            with open(item) as settings_file:
-                settings = json.load(settings_file)
-                response = airtable.search('App Name', settings['App Name'])
-                if len(response) > 0:  # delete first
-                    airtable.delete_by_field('App Name', settings['App Name'])
-                airtable.insert(settings)
+            with open(item) as schema_json_file:
+                schema = json.load(schema_json_file)
+                schemas[os.path.splitext(item.name)[0]] = schema
+
+    for os_name in schemas:
+        airtable = airtables[os_name]
+        for item in Path(f'settings/{os_name}').iterdir():
+            if item.is_file():
+                with open(item) as settings_file:
+                    settings = json.load(settings_file)
+                    response = airtable.search('App Name', settings['App Name'])
+                    if len(response) > 0:  # delete first
+                        airtable.delete_by_field('App Name', settings['App Name'])
+                    airtable.insert(settings)
+
+
+if __name__ == "__main__":
+    main()
