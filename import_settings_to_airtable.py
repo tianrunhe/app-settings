7c0ed51143c56bef8e59d59ce0cc62eb483f11ce
Bruce Tian <tianrunhe@gmail.com>
2020-05-16 16:45:36 -0700

    First commit for inserting into AirTable

---
+++ import_settings_to_airtable.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+from pathlib import Path
+from airtable import Airtable
+
+airtables = {
+    'iPhone': Airtable(os.getenv('AIRTABLE_BASE_ID'), 'iPhone'),
+    'iPad': Airtable(os.getenv('AIRTABLE_BASE_ID'), 'iPhone'),
+    'Mac': Airtable(os.getenv('AIRTABLE_BASE_ID'), 'iPhone')
+}
+
+schemas = {}
+for item in Path('schema').iterdir():
+    if item.is_file():
+        with open(item) as schema_json_file:
+            schema = json.load(schema_json_file)
+            schemas[os.path.splitext(item.name)[0]] = schema
+
+for os_name in schemas:
+    airtable = airtables[os_name]
+    for item in Path(f'settings/{os_name}').iterdir():
+        if item.is_file():
+            with open(item) as settings_file:
+                settings = json.load(settings_file)
+                response = airtable.search('App Name', settings['App Name'])
+                if len(response) > 0:  # delete first
+                    airtable.delete_by_field('App Name', settings['App Name'])
+                airtable.insert(settings)
